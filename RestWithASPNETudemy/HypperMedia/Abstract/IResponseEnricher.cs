5b42c40ccc50f6d95f269523b9522668928d59b4
Tiagomunerato <tiagomunerato@gmail.com>
2021-03-06 05:39:04 -0800

    CustonSerialization

---
+++ RestWithASPNETudemy/HypperMedia/Abstract/IResponseEnricher.cs
@@ -0,0 +1,10 @@
+﻿namespace RestWithASPNET.HypperMedia.Abstract
+{
+    public interface IResponseEnricher
+    {
+        bool CanEnrich(ResultExecutedContext context);
+
+        Task Enrich(ResultExecutedContext context);
+
+    }
+}
