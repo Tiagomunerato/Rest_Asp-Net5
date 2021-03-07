cec41936b4706c0c2f07609e690e154ba2cfbcb3
TiagoMunerato <tiagomunerato@gmail.com>
2021-03-07 05:14:05 -0800

    AddHateoas

---
+++ 12_RestWithASPNET_HATEOAS/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -0,0 +1,21 @@
+﻿using RestWithASPNETUdemy.HyperMedia;
+using RestWithASPNETUdemy.HyperMedia.Abstract;
+
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Data.VO
+{
+    public class PersonVO : ISupportHyperMedia
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Gender { get; set; }
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
+    }
+}
---
+++ 12_RestWithASPNET_HATEOAS/RestWithASPNETUdemy/RestWithASPNETUdemy/HyperMedia/Constants/RelationType.cs
@@ -0,0 +1,17 @@
+﻿namespace RestWithASPNETUdemy.HyperMedia.Constants
+{
+    public sealed class RelationType
+    {
+        public const string self = "self";
+        public const string post = "post";
+        public const string put = "put";
+        public const string delete = "delete";
+        public const string patch = "patch";
+        public const string next = "next";
+        public const string previous = "previous";
+        public const string first = "first";
+        public const string lats = "last";
+
+
+    }
+}
